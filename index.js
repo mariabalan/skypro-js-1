ca55c80e4f31f5fc3700710c47ac680ad7bf4e9f
Maria Balan <mane4ka.b@mail.ru>
2021-11-17 22:47:47 +0300

    hm1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 "use strict";
 let product = "Кресло";
 let price = "28 000";
-let discription = product + ', цена ' + price + ' рублей';
+let discription = `${product}, цена ${price} рублей`;
 alert(discription);
